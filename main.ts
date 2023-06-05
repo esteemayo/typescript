ef473d950ec5ba176c00bcf52acd014142664d09
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-05 03:13:54 +0100

    fix: handle variable declaration in ts

--- main.ts
+++ main.ts
@@ -6,3 +6,10 @@ console.log(message);
 let isBeginner: boolean = true;
 let total: number = 0;
 let name: string = 'John';
+
+let sentence = `
+  My name is ${name},
+  I'm a beginner is TypeScript
+`;
+
+console.log(sentence);
